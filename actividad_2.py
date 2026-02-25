62f0de985a3f737478d681689783b2a4f899bee5
Santiago Marin <santiago.marinb@upb.edu.co>
2026-02-25 15:33:01 -0500

    Agregué archivos

---
+++ actividad_2.py
@@ -0,0 +1,6 @@
+base = input("Ingrese la base:")
+base = float(base)
+altura = input("Ingrese la altura")
+altura = float(altura)
+area = base * altura / 2
+print("El area del triángulo es",area)
---
+++ primer python.py
@@ -0,0 +1,13 @@
+a = 34
+b = 45
+
+#A continuación creo un condicional
+if a > b:
+    print ("a es mayor que b")
+else:
+    print("a es menor que b")   
+
+# Un bucle que imprimo 100 veces la palabra UPB 
+for i in range(100):
+    print ("UPB")
+
