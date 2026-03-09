3699a45afb7e3ee8abe398f6c4d02fb398abac4a
Santiago Marin <santiago.marinb@upb.edu.co>
2026-03-09 10:34:42 -0500

    Edité las carpetas

---
+++ Actividad 2- De algoritmo a código fuente/actividad_2.py
@@ -0,0 +1,15 @@
+#Actividad:
+#Toma un pseudocódigo de un ejercicio de la unidad
+#anterior o escribe tu propio pseudocódigo
+
+base = input("Ingrese la base:")
+base = float(base)
+altura = input("Ingrese la altura")
+altura = float(altura)
+area = base * altura / 2
+print("El area del triángulo es",area)
+
+#Actividad:
+#Escribe tu propio pseudocódigo para calcular el 
+#promedio de 5 edades de tus compañeros. Luego tradúcelo a Python.
+Inicio  
--- actividad_2.py
+++
@@ -1,6 +0,0 @@
-base = input("Ingrese la base:")
-base = float(base)
-altura = input("Ingrese la altura")
-altura = float(altura)
-area = base * altura / 2
-print("El area del triángulo es",area)
